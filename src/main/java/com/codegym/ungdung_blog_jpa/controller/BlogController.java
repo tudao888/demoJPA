a40d51e861433fb7638dfb7e663c636a70d28fd6
tudao888 <daotu2106@gmail.com>
2023-01-30 22:01:26 +0700

    done CRUD

--- src/main/java/com/codegym/ungdung_blog_jpa/controller/BlogController.java
+++ src/main/java/com/codegym/ungdung_blog_jpa/controller/BlogController.java
@@ -5,13 +5,11 @@ import com.codegym.ungdung_blog_jpa.model.Category;
 import com.codegym.ungdung_blog_jpa.service.BlogService;
 import com.codegym.ungdung_blog_jpa.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -32,9 +30,9 @@ public class BlogController {
     }
 
     @GetMapping("/blogs")
-    public ModelAndView getAll() {
+    public ModelAndView getAll(@RequestParam (defaultValue = "0") int page) {
         ModelAndView modelAndView = new ModelAndView("show");
-        modelAndView.addObject("blogs", blogService.findAll());
+        modelAndView.addObject("blogs", blogService.findAll(PageRequest.of(page,2)));
         return modelAndView;
     }
 
@@ -56,7 +54,37 @@ public class BlogController {
         }
         blog.setImg("/image/"+ nameFile);
         blogService.save(blog);
-        modelAndView = new ModelAndView("redirect: /blogs") ;
+        modelAndView = new ModelAndView("redirect:/blogs") ;
         return  modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView delete(@PathVariable int id){
+        ModelAndView modelAndView = new ModelAndView("redirect:/blogs");
+        blogService.deleteById(id);
+        return modelAndView;
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView edit(@ModelAttribute("category") Category category, @PathVariable int id){
+        ModelAndView modelAndView = new ModelAndView("edit");
+        modelAndView.addObject("blog", blogService.findById(id).get());
+        return modelAndView;
+    }
+
+    @PostMapping("/edit")
+    public ModelAndView update(Blog blog, @RequestParam Integer idCategory, @RequestParam MultipartFile upImg){
+        ModelAndView modelAndView = new ModelAndView("redirect:/blogs");
+        Category category = categoryService.findById(idCategory).get();
+        blog.setCategory(category);
+        String nameFile = upImg.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(upImg.getBytes(), new File("E:\\Module 4\\ungDung_Blog_JPA\\src\\main\\webapp\\WEB-INF\\image/" + nameFile));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        blog.setImg("/image/"+ nameFile);
+        blogService.save(blog);
+        return modelAndView;
     }
 }
--- src/main/java/com/codegym/ungdung_blog_jpa/service/BlogService.java
+++ src/main/java/com/codegym/ungdung_blog_jpa/service/BlogService.java
@@ -3,9 +3,13 @@ package com.codegym.ungdung_blog_jpa.service;
 import com.codegym.ungdung_blog_jpa.model.Blog;
 import com.codegym.ungdung_blog_jpa.repository.IBlogRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BlogService{
@@ -13,11 +17,19 @@ public class BlogService{
     @Autowired
     IBlogRepo iBlogRepository;
 
-    public List<Blog> findAll(){
-        return (List<Blog>) iBlogRepository.findAll();
+    public Page<Blog> findAll(Pageable pageable){
+        return iBlogRepository.findAll(pageable);
     }
 
     public void save(Blog blog) {
         iBlogRepository.save(blog);
+    }
+
+    public void deleteById(int id) {
+        iBlogRepository.deleteById(id);
+    }
+
+    public Optional<Blog> findById(int id) {
+        return iBlogRepository.findById(id);
     }
 }
